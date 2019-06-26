7c46839881435c1c0ab53a547ceee2459cc9508b
Keithan <keithan.phil@gmail.com>
2019-06-26 23:49:11 +0200

    added json server with mock api

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -21,17 +21,12 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../src/index.html"));
 });
 
-app.get("/users", (reg, res) => {
-  //Hard coding for simplicity. Pretend this hits a real database
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity. Pretend this hits a real database
   res.json([
-    { id: 1, firstName: "Bob", lastName: "Smith", email: "bob@gmail.com" },
-    {
-      id: 2,
-      firstName: "Tammny",
-      lastName: "Norton",
-      email: "tnorton@gmail.com"
-    },
-    { id: 3, firstName: "Tina", lastName: "Lee", email: "tina@gmail.com" }
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
   ]);
 });
 
